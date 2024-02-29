d21013f1ccb16533ee855d8f936a9f6a3c6b8f64
Thibault Burtinovic <thibault.burtinovic@gmail.com>
2024-02-29 14:52:46 +0100

    add test gauge

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <div>
+  <div class="home">
     <div class="homeWrapper">
       <h1>Stress Button !</h1>
       <img :class="{ 'button': !isClicked, 'buttonHide': isClicked }" id="buttonNotClicked" src="@/assets/button.svg" alt="Bouton"
@@ -10,8 +10,11 @@
         draggable="false"  @click="toggleButtonClick">
     </div>
     <div class="count">
-      <p>Nombre de clics : {{ clickCount }}</p>
+      <p>Nombre de clics sec : {{ clicksPerSecond }}</p>
       <p>Timer : {{ timerCount }}</p>
+    </div>
+    <div  class="gauge">
+        <div class="gaugeFill" :style="{ width: (clicksPerSecond * 20) + 'px' }"></div>
     </div>
   </div>
 </template>
@@ -26,7 +29,9 @@ export default {
       isClicked: false,
       clickCount: 0, // Propriété pour stocker le nombre de clics
       isCounting: false,
-      timerCount: 0
+      timerCount: 0,
+      clicksPerSecond: 0, // Propriété pour stocker le nombre de clics par seconde
+      clicksInLastUpdate: 0
     };
   },
   methods: {
@@ -41,16 +46,24 @@ export default {
     startCounting() {
       const timer = setInterval(() => {
         this.timerCount++; // Incrémente le temps écoulé à chaque seconde
-        if (this.timerCount >= 5) {
-          clearInterval(timer); // Arrête le comptage après 5 secondes
-          this.isCounting = false; // Réinitialise le compte après 5 secondes
-          this.clickCount = 0;
+        this.clicksPerSecond = this.clickCount; // Met à jour le nombre de clics par seconde
+        this.clickCount = 0; // Réinitialise le nombre de clics
+        if (this.timerCount >= 10) {
+          clearInterval(timer); // Arrête le comptage après 10 secondes
+          this.isCounting = false; // Réinitialise le compte après 10 secondes
           this.timerCount = 0;
+          this.clicksPerSecond = 0; // Réinitialise le nombre de clics par seconde
         }
       }, 1000); // Met à jour timerCount chaque seconde
+    },
+    updateClickPerSecond() {
+      // Met à jour le nombre de clics par seconde toutes les 0.1 secondes
+      setInterval(() => {
+        this.clickPerSecond = this.clicksInLastUpdate / 0.1;
+        this.clicksInLastUpdate = 0; // Réinitialise le nombre de clics depuis le dernier rafraîchissement
+      }, 300);
     }
   }
 };
 </script>
-  
   
